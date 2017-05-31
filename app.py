8130122c367ced88fe7ea6e8c6ff07752e7b08f4
Bob Corboy <r.corboy23@gmail.com>
2017-05-31 09:14:14 -0600

    functioning, yo

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('#about')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
